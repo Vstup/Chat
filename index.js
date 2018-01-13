40d224973fc5c68bcc7d09dbb9149de01b12f283
MishaHnatyshyn <gnatyshynmisha@gmail.com>
2018-01-13 14:13:00 +0200

    node-static fix

--- index.js
+++ index.js
@@ -37,12 +37,20 @@ server.on('request', function (req, res) {
   const data = url.parse(req.url, true).query;
   const path = url.parse(req.url, true).pathname;
   // console.log(path);
+    if (path === '/styles/style.css' || path === '/scripts/common.js'){
+        req.addListener( 'end', function () {
+
+            fileServer.serve( req, res );
+
+        } ).resume()
+    }
 
   if (path === '/' || path === '/home' ) {
     console.log('checkSess: ' + funcs.checkSess(req));
     if ( !funcs.checkSess(req) ) {
       res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
       res.end(fs.readFileSync('login.html'));
+
 
     } else {
       res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
@@ -113,9 +121,7 @@ server.on('request', function (req, res) {
 
   }
 
-  /* req.addListener( 'end', function () {
-    fileServer.serve( req, res );
-  } ).resume();*/
+
 });
 
 server.listen(port, function () {
--- public/scripts/common.js
+++ public/scripts/common.js
@@ -1,29 +1,74 @@
 'use strict';
+let nameCheck = getCookie();
+let userLi;
 
-let currentChat = '';
-const socket = io({transports: ['websocket']});
 
-socket.on('broadcast', function (data) {
-    render(data);
-    scrolldown();
-});
+window.onload = () => {
+  document.getElementById('u-name').innerHTML = getCookie();
+  document.getElementById('left').style.height = height - 40 + 'px';
+  document.getElementById('right').style.height = height -40 + 'px';
+  document.getElementById('rightBefore').style.height = height -40 + 'px';
+  document.getElementById('chat').style.height = height - 100 + 'px';
+  document.getElementById('left-up').style.height = height - 153 + 'px';
+  document.getElementById('searchRes').style.height = height - 166 + 'px';
+  document.getElementById('messages').style.height = height - 153 + 'px';
+  const block = document.getElementById('messages');
 
-socket.on('clear1', function () {
-    document.getElementById('messages').innerHTML = '<div></div>';
-});
+};
+function getUserLi(value,callback) {
 
+  const xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      userLi = JSON.parse(this.responseText);
+      console.log('touched');
+      callback(value);
+    }
+  };
+  xhr.open('GET', '/index.js?uname=', true);
+  xhr.setRequestHeader('cause', 'search');
+  xhr.send();
+
+}
+
+function serchRes(value) {
+  let res = '';
+  if (value === '') {
+    document.getElementById('searchRes').innerHTML = '<p>No users found</p>';
+    document.getElementById('left-up').style.display = 'block';
+    document.getElementById('searchRes').style.display = 'none';
+  } else {
+
+    for (let i = 0; i < userLi.length; i++) {
+      if (userLi[i].indexOf(value) === 0) {
+        res += '<div class="searchedItemContainer" ' +
+                    'onclick="chat(\'' + userLi[i] + '\')">' + userLi[i] + '</div>\n';
+      }
+    }
+    document.getElementById('searchRes').innerHTML = res;
+    document.getElementById('left-up').style.display = 'none';
+    document.getElementById('searchRes').style.display = 'block';
+  }
+}
+
+function search(value) {
+  if (!userLi){
+    getUserLi(value, serchRes);
+  } else serchRes(value);
+
+}
 
 const height = document.documentElement.clientHeight;
-document.getElementById('left').style.height = height - 40 + 'px';
-document.getElementById('right').style.height = height -40 + 'px';
-document.getElementById('chat').style.height = height - 100 + 'px';
-document.getElementById('left-up').style.height = height - 100 + 'px';
-document.getElementById('messages').style.height = height - 144 + 'px';
-const block = document.getElementById('messages');
+
 
 function scrolldown() {
-  block.scrollTop = block.scrollHeight;
+  // block.scrollTop = block.scrollHeight;
 }
+
+let currentChat = '';
+
+let displayFlag = false;
 
 function getCookie() {
   const   matches = document.cookie.match(new RegExp(
@@ -32,14 +77,22 @@ function getCookie() {
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
-function chat(user2) {
+const chat = (user2) => {
+  userLi = '';
   const xhr = new XMLHttpRequest();
 
   xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       const data = JSON.parse(this.responseText);
-      document.getElementById('left-up').innerHTML = '<button onclick="singOut()">Sing Out</button>' +
-                data[0] + '<hr>' + data[1];
+
+      document.getElementById('left-up').innerHTML = data[1];
+      document.getElementById('searchField').value = '';
+      document.getElementById('searchRes').innerHTML = '<p>No users found</p>';
+      document.getElementById('left-up').style.display = 'block';
+      document.getElementById('searchRes').style.display = 'none';
+      goToChat(data[0]);
+
+      document.getElementById('left-up').innerHTML = data[0] + data[1];
     }
   };
   xhr.open('POST', '/index.js?uname='+user2, true);
@@ -48,8 +101,27 @@ function chat(user2) {
 
 };
 
-function goToChat(chatId) {
+const goToChat = (chatId) => {
+
+  const userWith = document.getElementById(chatId).innerHTML;
+
+  document.getElementById('chat-header').innerHTML = userWith.replace('<br>','');
+
+  if (currentChat !== ''){
+    document.getElementById(currentChat).style =
+            document.getElementsByClassName('chatContainer').style;
+  }
+  //        document.getElementById(chatId).style.backgroundColor = '#ff5c00';
+  document.getElementById(chatId).style.backgroundColor = '#e4eaee';
+  document.getElementById(chatId).style.color = 'black';
+
   currentChat = chatId;
+
+  if(!displayFlag){
+    document.getElementById('right').style.display = 'block';
+    document.getElementById('rightBefore').style.display = 'none';
+    displayFlag = true;
+  }
   const xhr = new XMLHttpRequest();
 
   xhr.onreadystatechange = function () {
@@ -57,7 +129,14 @@ function goToChat(chatId) {
       const data = JSON.parse(this.responseText);
       let res = '';
       for (let i = 0; i<data.length;i++){
-        res += '<div>' + data[i].userSent + ': ' + data[i].messText + '</div>\n';
+
+        // res += '<div>' + data[i].userSent + ': ' + data[i].messText + '</div>\n';
+
+        if (data[i].userSent == nameCheck) {
+          res += '<div id="right-message">' + '<span class="message-decor">' + data[i].messText + '</span>' + '</div>\n';
+        } else {
+          res += '<div id="left-message">' + '<span class="message-decor">' + data[i].messText + '</span>' + '</div>\n';
+        }
       }
       document.getElementById('messages').innerHTML = res;
       window.scrollTo(0, document.body.scrollHeight);
@@ -70,7 +149,7 @@ function goToChat(chatId) {
   xhr.send();
 };
 
-function singOut() {
+const singOut = () => {
 
   const xhr = new XMLHttpRequest();
 
@@ -82,6 +161,17 @@ function singOut() {
   xhr.open('POST', '/logout', true);
   xhr.send();
 };
+
+const socket = io({transports: ['websocket']});
+
+socket.on('broadcast', function (data) {
+  render(data);
+  scrolldown();
+});
+
+socket.on('clear1', function () {
+  document.getElementById('messages').innerHTML = '<div></div>';
+});
 
 function sendMessage(nickname, message) {
 
@@ -96,9 +186,15 @@ function sendMessage(nickname, message) {
 
 }
 
+
 function render(data) {
   if (data.chatId === currentChat) {
-    document.getElementById('messages').innerHTML += '<div>' + data.nickname + ': ' + data.message + '</div>';
+    if (data.nickname == nameCheck) {
+      document.getElementById('messages').innerHTML += '<div id="right-message">' + '<span class="message-decor">' + '<span>' + data.message + '<span>' + '</span>' + '</div>';
+    } else {
+      document.getElementById('messages').innerHTML += '<div id="left-message">' + '<span class="message-decor">' + data.message + '</span>' + '</div>';
+    }
+
     window.scrollTo(0, document.body.scrollHeight);
   }
 }
