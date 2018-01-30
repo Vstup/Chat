dc768e5ba1882c84f85a3717270ab164dc2048e1
Vstup <34066277+Vstup@users.noreply.github.com>
2018-01-30 20:05:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+const http = require('http');
+const socketio = require('socket.io');
+const funcs = require('./server/funcs');
+const auth = require('./server/authorization');
+const router = require('./server/router');
+const db =require('./server/db');
+
+const server = http.createServer();
+const io = socketio(server);
+const port = 8081;
+
+io.on('connection', function (socket) {
+  socket.on('message', function (data) {
+    io.emit('broadcast', data);
+    funcs.messageLog(data.nickname,data.chatId,data.message);
+  });
+  socket.on('clear', function () {
+    io.emit('clear1' );
+  });
+});
+
+server.on('request', function (req, res) {
+  auth.checkSess(req, (access) => {
+    if (access) router.route(req, res);
+    else router.guest(req, res);
+  });
+});
+
+server.listen(port, function () {
+  console.log('Server running');
+  db.dbConnect();
+});
+
+
