a2817d9dac253133d5f11a815b4e2b58b4a207da
Vstup <34066277+Vstup@users.noreply.github.com>
2018-02-04 21:35:04 +0200

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,13 +1,13 @@
 'use strict';
 const MongoClient = require('mongodb').MongoClient;
 // const url = "mongodb://localhost/mydb"; 
-const url = "mongodb://chat:chat@172.30.78.118/mydb";
+const url = "mongodb://admin:admin@172.30.78.118/chat";
 let chatdb;
 
 
 const dbConnect = () => {
     MongoClient.connect(url, function (err, database) {
-        chatdb = database.db('mydb');
+        chatdb = database.db('chat');
         console.log('data base connected!');
         module.exports.chatdb = chatdb;
     });
